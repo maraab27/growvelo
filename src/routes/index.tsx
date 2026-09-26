8d7e2beadcf6fc66f5f5b01f069edb9fe269abb2
maraab27 <abdullah20050127@gmail.com>
2026-09-26 13:09:26 +0600

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -26,7 +26,7 @@ export const Route = createFileRoute("/")({
         rel: "preload",
         as: "image",
         type: "image/jpeg",
-        href: "https://zqjgnoycsiwwcklapelt.supabase.co/storage/v1/object/public/gallery/course_thumb_video_editing_batch_3_1790404767135.webp",
+        href: "https://zqjgnoycsiwwcklapelt.supabase.co/storage/v1/object/public/gallery/course_thumb_video_editing_batch_3_1790128805987.jpg",
         fetchPriority: "high",
       },
     ],
